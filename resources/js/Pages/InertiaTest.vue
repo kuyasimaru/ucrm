9e952fd91ca65efe85ea64b006562b7573220ff1
iinuma_jinto <rfanzhao@gmail.com>
2024-07-16 22:51:39 +0900

    inertia-vue.js

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+Inertiaテスト<br>
+<a href="/">aタグ経由</a><br>
+<link href="/">読み込まない</link>
+<Link href="/">Linkタグ経由</Link><br>
+<Link :href="route('inertia.index')">確認</Link><br>
+<Link :href="route('inertia.show', { id:50 })">ルートshowテスト</Link>
+
+</template>
